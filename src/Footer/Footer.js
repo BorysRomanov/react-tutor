857e029b82dd4d812d87321f6306ae105ee7e78b
B <boris.romanov.91@mail.ru>
2017-03-06 18:11:27 +0200

    Hello World & Footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import style from './Footer.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">Footer</div>
+    )
+  }
+}
